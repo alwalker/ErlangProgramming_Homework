0adc0c9443e7eeb43481ce4a1a067193b46d94ca
Andrew Walker <alwalker21@gmail.com>
2013-08-31 11:59:03 -0500

    Added concatenate to 3-5

--- ch3/threefive.erl
+++ ch3/threefive.erl
@@ -1,5 +1,5 @@
 -module(threefive).
--export([filter/2, reverse/1]).
+-export([filter/2, reverse/1, concatenate/1]).
 
 filter(Int, Ints) -> 
 	Result = filter(Int, Ints, []),
@@ -12,3 +12,11 @@ reverse(List) -> reverse(List, []).
 reverse([], NewList) -> NewList;
 reverse([Head | Tail], NewList) -> reverse(Tail, [Head | NewList]).
 
+concatenate(List) -> 
+	Result = concatenate(List, []),
+	reverse(Result).
+concatenate([], NewList) -> NewList;
+concatenate([Head | Tail], NewList) -> concatenate(Tail, add_to_list(Head, NewList)).
+
+add_to_list([], NewList) -> NewList;
+add_to_list([Head | Tail], NewList) -> add_to_list(Tail, [Head | NewList]).
