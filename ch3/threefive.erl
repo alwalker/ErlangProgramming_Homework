13a32234b3e702ac0160c63a6101713fdf38900f
Andrew Walker <alwalker21@gmail.com>
2013-08-31 11:44:44 -0500

    Cleaned up first two problems

--- ch3/threefive.erl
+++ ch3/threefive.erl
@@ -1,13 +1,14 @@
 -module(threefive).
 -export([filter/2, reverse/1]).
 
-filter(Int, Ints) -> leq_filter(Int, Ints, []).
+filter(Int, Ints) -> 
+	Result = filter(Int, Ints, []),
+	reverse(Result).
+filter(_, [], Filtered) -> Filtered;
+filter(Int, [Head | Tail], Filtered) when Head =< Int -> filter(Int, Tail, [Head | Filtered]);
+filter(Int, [_ | Tail], Filtered) -> filter(Int, Tail, Filtered).   
 
 reverse(List) -> reverse(List, []).
-
 reverse([], NewList) -> NewList;
 reverse([Head | Tail], NewList) -> reverse(Tail, [Head | NewList]).
 
-leq_filter(_, [], Filtered) -> Filtered;
-leq_filter(Int, [Head | Tail], Filtered) when Head =< Int -> leq_filter(Int, Tail, [Head | Filtered]);
-leq_filter(Int, [_ | Tail], Filtered) -> leq_filter(Int, Tail, Filtered). 
